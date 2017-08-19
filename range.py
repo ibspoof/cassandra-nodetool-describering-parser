31ae219988665bae55fd550d4a749b45a04ec6bd
Brad Vernon <brad.vernon@datastax.com>
2017-08-19 12:21:12 -0700

    Added token range size statistics and updated the output to show racks

--- range.py
+++ range.py
@@ -13,6 +13,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 DATA_CENTERS = []
+RANGE_SIZES = {}
 REGEX_START_END_COMP = re.compile(r'.*TokenRange\(start_token:(.*), end_token:(.*), '
                                   r'endpoints:\[(.*)\], rpc_.* endpoint_details:\[(.*)]')
 REGEX_ENDPNT_COMP = re.compile(r'EndpointDetails\(host:(.*), datacenter:(.*), rack:(.*)')
@@ -38,6 +39,7 @@ if __name__ == """__main__""":
         token_details['token_start'] = st_end_res.group(1)
         token_details['token_end'] = st_end_res.group(2)
         token_details['ip'] = st_end_res.group(3).split(", ")[0]
+        token_details['size'] = abs(long(token_details['token_end']) - long(token_details['token_start']))
 
         replicas = st_end_res.group(4)
 
@@ -54,8 +56,14 @@ if __name__ == """__main__""":
 
         token_details['replicas'] = replica_info
 
+        if token_details['dc'] not in RANGE_SIZES:
+            RANGE_SIZES[token_details['dc']] = [token_details['size']]
+        else:
+            RANGE_SIZES[token_details['dc']].append(token_details['size'])
+
         if token_details['dc'] not in DATA_CENTERS:
             DATA_CENTERS.append(token_details['dc'])
+
 
         CLUSTER_DETAILS[token_details['ip']] = token_details
 
@@ -66,15 +74,27 @@ if __name__ == """__main__""":
             res = REGEX_START_END_COMP.match(line)
             if line.find(node + ",") > -1:
                 if res.group(3).split(", ")[0] != CLUSTER_DETAILS[node]['ip']:
-                    secondary_ranges.append({'ip': res.group(3).split(", ")[0], 'token_start': res.group(1), 'token_end': res.group(2)})
+                    secondary_rep = {'ip': res.group(3).split(", ")[0], 'token_start': res.group(1),
+                                     'token_end': res.group(2)}
+                    secondary_rep['rack'] = CLUSTER_DETAILS[secondary_rep['ip']]['rack']
+                    secondary_ranges.append(secondary_rep)
                 found_cnt += 1
 
         CLUSTER_DETAILS[node]['range_cnt'] = found_cnt
         CLUSTER_DETAILS[node]['neighbors'] = secondary_ranges
 
     for dc in DATA_CENTERS:
+
+
+        # avg_range_size = sum(RANGE_SIZES[dc]) / len(RANGE_SIZES[dc])
+        avg_range_size = float(sum(RANGE_SIZES[dc])) / max(len(RANGE_SIZES[dc]), 1)
+        sorted_range = sorted(RANGE_SIZES[dc])
+        smallest = sorted_range[0]
+        largest = sorted_range[len(sorted_range) - 1]
+
         print "Datacenter: %s" % dc
         print ""
+
         for node in CLUSTER_DETAILS:
             if CLUSTER_DETAILS[node]['dc'] == dc:
                 cur_node = CLUSTER_DETAILS[node]
@@ -82,12 +102,23 @@ if __name__ == """__main__""":
                 linked_nodes = []
                 linked_ranges = []
                 for n in cur_node['neighbors']:
-                    linked_nodes.append(n['ip'])
-                    linked_ranges.append("[%s, %s]" % (n['token_start'], n['token_end']))
+                    linked_nodes.append("%s (%s)" % (n['ip'], n['rack']))
+                    linked_ranges.append("%s: [%s, %s]" % (n['ip'], n['token_start'], n['token_end']))
+
+                diff_percent = abs((float(cur_node['size'] - float(avg_range_size))) / float(avg_range_size)) * 100.0
 
                 print "  %s" % node
                 print "\t- Total Ranges: %s" % (cur_node['range_cnt'])
                 print "\t- Primary Range: [%s, %s]" % (cur_node['token_start'], cur_node['token_end'])
+                print "\t- Primary Range Size: %s    (Diff from avg: %d%%)" % (cur_node['size'], diff_percent)
                 print "\t- Secondary Nodes: %s" % ", ".join(linked_nodes)
-                print "\t- Secondary Ranges: %s" % ", ".join(linked_ranges)
+                print "\t- Secondary Ranges: "
+                for r in linked_ranges:
+                    print "\t\t%s" % r
                 print ""
+
+        print "  Token range sizes:"
+        print "\tSmallest: %d" % smallest
+        print "\tLargest: %d" % largest
+        print "\tAverage: %d" % avg_range_size
+
